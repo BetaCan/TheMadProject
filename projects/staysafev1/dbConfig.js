47b9ba658a34bbc903d1fc881f98899fd5c1a26c
Canute <93085939+BetaCan@users.noreply.github.com>
2025-02-28 22:04:43 +0000

    Update dbConfig.js

--- projects/staysafev1/dbConfig.js
+++ projects/staysafev1/dbConfig.js
@@ -1,10 +1,10 @@
 const dbConfig = {
-  database: process.env.DB_NAME || 'staysafev1db',
+  database: process.env.DB_NAME || 'test',
   port: process.env.DB_PORT || 3306,
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PSWD || '',
   namedPlaceholders: true,
-};
+}
 
-export default dbConfig;
+export default dbConfig
